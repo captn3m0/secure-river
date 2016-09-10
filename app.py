afbe6c422651d7c3767de558b23e95b2ec31bcef
Abhay Rana <me@captnemo.in>
2016-09-10 18:12:43 +0530

    [routes] Adds scaffold code for our routes

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import network
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/jobs')
+def fetch_jobs():
+    # returns the network entity
+    network = network.get_network()
+    return models.job.find_by_network(network).json()
+
+@app.route('/jobs/<id>')
+def fetch_job_details(id):
+    return models.job.find_by_id(id).json()
+
+@app.route('/network/<id>')
+def fetch_network_reports(id):
+    return models.network.find_by_id(id).reports().json()
+
+@app.route('/jobs/<id>/submit')
+def submit_job_response(id):
+    job = models.job.find(id)
--- network.py
+++ network.py
@@ -9,3 +9,10 @@ class Network(object):
   def get_isp_info(ip):
     url = "http://ip-api.com/json/" + ip
     return requests.get(url).json()
+
+  def get_network_id(data):
+    if (data['type'] == 'mobile'):
+      # TODO: split it
+      self.lookup_mcc_mnc(data['mccmnc'])
+    elif(data['type'] == 'wifi'):
+      self.get_isp_info(data.ip)
