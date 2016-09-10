e384271b2004c220fe27fe7990a5e7ea602c1777
Abhay Rana <me@captnemo.in>
2016-09-10 19:01:36 +0530

    Fixes routes

--- app.py
+++ app.py
@@ -6,42 +6,40 @@ from flask import g
 from os.path import abspath, dirname
 import sys
 
-import network as network_util
-
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.before_request(network_util.middleware)
 
 sys.path.append(abspath(dirname(__file__)))
 
 import models
+import network as network_util
 
-@app.route('/jobs', methods['GET'])
+app.before_request(network_util.Network.middleware)
+
+@app.route('/jobs', methods=['GET'])
 def fetch_jobs():
-    # returns the network entity
-    network = network.get_network()
+    print(g.networks)
     return models.job.find_by_network(network).json()
 
-@app.route('/jobs/<id>', methods['GET'])
+@app.route('/jobs/<id>', methods=['GET'])
 def fetch_job_details(id):
     return models.job.find_by_id(id).json()
 
-@app.route('/network/<id>', methods['GET'])
+@app.route('/network/<id>', methods=['GET'])
 def fetch_network_reports(id):
     return models.network.find_by_id(id).reports().json()
 
-@app.route('/jobs/<id>/submit', methods['POST'])
+@app.route('/jobs/<id>/submit', methods=['POST'])
 def submit_job_response(id):
     job = models.job.find(id)
 
-@app.route('/clients', methods['POST'])
+@app.route('/clients', methods=['POST'])
 def create_client():
     job = models.client.register()
-
 
 @app.route('/')
 def hello_world():
